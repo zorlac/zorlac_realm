53a79b397c83414f10d229855226a45f37c80b82
Richard Teston <zorlac.realm@gmail.com>
2014-04-25 09:55:41 +0800

    First commit

---
+++ GitSandbox/src/xam/sandbox/git/GitTest.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package xam.sandbox.git;
+
+/**
+ * @author rmteston
+ *
+ */
+public class GitTest {
+	
+	public static void main(String[] args) {
+		System.out.println("Git test");
+	}
+
+}
